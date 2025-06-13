85ffd0eb82c4c3236be75289e6b35fb2e953a7c1
a7777611 <gianna_611@163.com>
2025-06-13 22:18:04 +0800

    complete send_and_receive method

--- UDPclient.py
+++ UDPclient.py
@@ -1,4 +1,19 @@
 import socket
+import base64
+import os
+import time
+
+def send_and_receive(sock, msg, addr, max_retries=5):
+    for attempt in range(max_retries):
+        sock.sendto(msg.encode(), addr)
+        sock.settimeout(2 + attempt * 2) #动态超时
+        try:
+            data, _ = sock.recvform(2048)
+            return data.decode()
+        except socket.timeout:
+            print(f"Timeout, retrying...({attempt + 1}/{max_retries})")
+    raise Exception("Max retries exceeded")
+
 
 def main():
     host = input("Enter server host: ")
